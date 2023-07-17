ea074a9f7f97e5669c882043d8a75f5781cb6ab8
William Chau <william.chau@databricks.com>
2023-07-17 12:00:47 -0700

    v0.1-beta.5

--- Clean Room Orchestrator Job Step 1.py
+++ Clean Room Orchestrator Job Step 1.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-# MAGIC %pip install git+https://github.com/databricks/clean-room-orchestration -q
+# MAGIC %pip install git+https://github.com/databricks/clean-room-orchestration@v0.1-beta.5 -q
 # MAGIC from databricks_clean_room_orchestrator.client import CleanRoomClient
 
 # COMMAND ----------
